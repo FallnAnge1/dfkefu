30fd7e65e1a72821d67e092bee4024194ad24c47
FallenAngel <723897894@qq.com>
2026-05-17 05:38:12 +0800

    Clear repository contents

--- find_modal.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const c = fs.readFileSync('C:/Users/DFAI/Desktop/dfkefu_new/index.html', 'utf8');
-const idx = c.indexOf('style="display:none; position:fixed; inset:0; z-index:9999');
-const end = c.indexOf('</div>', idx);
-console.log(c.substring(idx - 50, end + 7));
--- find_modal2.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const c = fs.readFileSync('C:/Users/DFAI/Desktop/dfkefu_new/index.html', 'utf8');
-const idx = c.indexOf('<!-- 联系方式弹窗 -->');
-const end = c.indexOf('</div>\n\n<script>', idx);
-console.log(c.substring(idx, end + 7));
